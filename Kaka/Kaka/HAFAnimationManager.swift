4ad24fd3afe73726a050eaae0308fc86c4d675fc
HsiangHo <ho_cheung@qq.com>
2018-08-15 11:38:51 +0800

    add HAFAnimationManager and HAFKakaObject

---
+++ Kaka/Kaka/HAFAnimationManager.swift
@@ -0,0 +1,93 @@
+//
+//  HAFAnimationManager.swift
+//  Kaka
+//
+//  Created by Jovi on 8/11/18.
+//  Copyright © 2018 Jovi. All rights reserved.
+//
+
+import Cocoa
+
+class HAFAnimationManager: NSObject {
+    static let sharedManager = HAFAnimationManager()
+    
+    var arrogance: HAFAnimationSequence;
+    var bomb: HAFAnimationSequence;
+    var bye: HAFAnimationSequence;
+    var drag1: HAFAnimationSequence;
+    var drag2: HAFAnimationSequence;
+    var drag3: HAFAnimationSequence;
+    var eatWatermelon: HAFAnimationSequence;
+    var grimace: HAFAnimationSequence;
+    var happy: HAFAnimationSequence;
+    var hidden1: HAFAnimationSequence;
+    var hidden2: HAFAnimationSequence;
+    var hidden3: HAFAnimationSequence;
+    var hidden4: HAFAnimationSequence;
+    var hidden5: HAFAnimationSequence;
+    var knife1: HAFAnimationSequence;
+    var knife2: HAFAnimationSequence;
+    var knife3: HAFAnimationSequence;
+    var scan1: HAFAnimationSequence;
+    var scan2: HAFAnimationSequence;
+    var scan3: HAFAnimationSequence;
+    var scissor: HAFAnimationSequence;
+    var scratchHead: HAFAnimationSequence;
+    var showTip: HAFAnimationSequence;
+    var sleep1: HAFAnimationSequence;
+    var sleep2: HAFAnimationSequence;
+    var sleep3: HAFAnimationSequence;
+    var smog1: HAFAnimationSequence;
+    var smog2: HAFAnimationSequence;
+    var stand: HAFAnimationSequence;
+    
+    var randomAnimationSequenceArray: Array<HAFAnimationSequence> = Array<HAFAnimationSequence>()
+    
+    init(animationDir: String = Bundle.main.bundlePath.appending("/Contents/Resources/Animations")) {
+        arrogance = HAFAnimationSequence.init(animationDir.appending("/Arrogance"), animationName: "Arrogance")
+        bomb = HAFAnimationSequence.init(animationDir.appending("/Bomb"), animationName: "Bomb")
+        bye = HAFAnimationSequence.init(animationDir.appending("/Bye"), animationName: "Bye")
+        drag1 = HAFAnimationSequence.init(animationDir.appending("/Drag1"), animationName: "Drag1")
+        drag2 = HAFAnimationSequence.init(animationDir.appending("/Drag2"), animationName: "Drag2")
+        drag3 = HAFAnimationSequence.init(animationDir.appending("/Drag3"), animationName: "Drag3")
+        eatWatermelon = HAFAnimationSequence.init(animationDir.appending("/EatWatermelon"), animationName: "EatWatermelon")
+        grimace = HAFAnimationSequence.init(animationDir.appending("/Grimace"), animationName: "Grimace")
+        happy = HAFAnimationSequence.init(animationDir.appending("/Happy"), animationName: "Happy")
+        hidden1 = HAFAnimationSequence.init(animationDir.appending("/Hidden1"), animationName: "Hidden1")
+        hidden2 = HAFAnimationSequence.init(animationDir.appending("/Hidden2"), animationName: "Hidden2")
+        hidden3 = HAFAnimationSequence.init(animationDir.appending("/Hidden3"), animationName: "Hidden3")
+        hidden4 = HAFAnimationSequence.init(animationDir.appending("/Hidden4"), animationName: "Hidden4")
+        hidden5 = HAFAnimationSequence.init(animationDir.appending("/Hidden5"), animationName: "Hidden5")
+        knife1 = HAFAnimationSequence.init(animationDir.appending("/Knife1"), animationName: "Knife1")
+        knife2 = HAFAnimationSequence.init(animationDir.appending("/Knife2"), animationName: "Knife2")
+        knife3 = HAFAnimationSequence.init(animationDir.appending("/Knife3"), animationName: "Knife3")
+        scan1 = HAFAnimationSequence.init(animationDir.appending("/Scan1"), animationName: "Scan1")
+        scan2 = HAFAnimationSequence.init(animationDir.appending("/Scan2"), animationName: "Scan2")
+        scan3 = HAFAnimationSequence.init(animationDir.appending("/Scan3"), animationName: "Scan3")
+        scissor = HAFAnimationSequence.init(animationDir.appending("/Scissor"), animationName: "Scissor")
+        scratchHead = HAFAnimationSequence.init(animationDir.appending("/ScratchHead"), animationName: "ScratchHead")
+        showTip = HAFAnimationSequence.init(animationDir.appending("/ShowTip"), animationName: "ShowTip")
+        sleep1 = HAFAnimationSequence.init(animationDir.appending("/Sleep1"), animationName: "Sleep1")
+        sleep2 = HAFAnimationSequence.init(animationDir.appending("/Sleep2"), animationName: "Sleep2")
+        sleep3 = HAFAnimationSequence.init(animationDir.appending("/Sleep3"), animationName: "Sleep3")
+        smog1 = HAFAnimationSequence.init(animationDir.appending("/Smog1"), animationName: "Smog1")
+        smog2 = HAFAnimationSequence.init(animationDir.appending("/Smog2"), animationName: "Smog2")
+        stand = HAFAnimationSequence.init(animationDir.appending("/Stand"), animationName: "Stand")
+        
+        randomAnimationSequenceArray.append(arrogance)
+        randomAnimationSequenceArray.append(bomb)
+        randomAnimationSequenceArray.append(eatWatermelon)
+        randomAnimationSequenceArray.append(grimace)
+        randomAnimationSequenceArray.append(happy)
+        randomAnimationSequenceArray.append(scissor)
+        randomAnimationSequenceArray.append(scratchHead)
+        randomAnimationSequenceArray.append(showTip)
+        randomAnimationSequenceArray.append(stand)
+        super.init()
+    }
+    
+    func randomAnimationSequence() -> HAFAnimationSequence {
+        let index:Int = Int(arc4random_uniform(UInt32(randomAnimationSequenceArray.count)))
+        return randomAnimationSequenceArray[index]
+    }
+}
---
+++ Kaka/Kaka/HAFKakaObject.swift
@@ -0,0 +1,74 @@
+//
+//  HAFKakaObject.swift
+//  Kaka
+//
+//  Created by Jovi on 8/12/18.
+//  Copyright © 2018 Jovi. All rights reserved.
+//
+
+import Cocoa
+
+enum UserActionType: Int {
+    case eInvalidAction = 0
+    case eLeftBtnClick
+    case eRightBtnClick
+    case eDoubleClick
+    case eDragToRightMargin
+    case eDragFromRightMargion
+    case eDragToTopMargin
+    case eDragFromTopMargin
+}
+
+enum KakaStateType {
+    case eKakaStateNormal
+    case eKakaStateDragging
+    case eKakaStateHidden
+}
+
+class HAFKakaObject: NSObject {
+    var _state: KakaStateType
+    var _nextAction: UserActionType
+    var _animationSequence: HAFAnimationSequence?
+    var _nextAnimationSequence: HAFAnimationSequence?
+    var _nCurrentFrameCount: Int
+    var _nCurrentFrameIndex: Int
+    
+    override init() {
+        _state = .eKakaStateNormal
+        _nextAction = .eInvalidAction
+        _nextAnimationSequence = nil
+        _animationSequence = HAFAnimationManager.sharedManager.happy
+        _nCurrentFrameCount = _animationSequence!.frameCount()
+        _nCurrentFrameIndex = 0
+        super.init()
+    }
+    
+    func doAction(actionType: UserActionType) -> Void {
+        if .eInvalidAction == _nextAction{
+            _nextAction = actionType
+        }
+    }
+    
+    func currentAnimationFrame() -> NSImage? {
+        if nil != _animationSequence && _nCurrentFrameIndex < _nCurrentFrameCount {
+            return _animationSequence!.frameAtIndex(_nCurrentFrameIndex)
+        }
+        return nil
+    }
+    
+    func moveToNextAnimationFrame() -> Void {
+        _nCurrentFrameIndex += 1;
+        if _nCurrentFrameIndex < _nCurrentFrameCount {
+            return;
+        }
+        
+        if nil == _nextAnimationSequence{
+            _animationSequence = HAFAnimationManager.sharedManager.randomAnimationSequence()
+        }else{
+            _animationSequence = _nextAnimationSequence
+            _nextAnimationSequence = nil;
+        }
+        _nCurrentFrameCount = _animationSequence!.frameCount()
+        _nCurrentFrameIndex = 0
+    }
+}
