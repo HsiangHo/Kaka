8d96421fb666c164bca8cd5ca11e74fa5453835b
HsiangHo <ho_cheung@qq.com>
2018-08-25 17:00:51 +0800

    added LaunchAtLoginHelper

---
+++ Kaka/LaunchAtLoginHelper/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  LaunchAtLoginHelper
+//
+//  Created by Jovi on 8/25/18.
+//  Copyright © 2018 Jovi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+        let mainAppIdentifier = "com.HyperartFlow.Kaka"
+        let runningApps = NSWorkspace.shared.runningApplications
+        let isRunning = !runningApps.filter { $0.bundleIdentifier == mainAppIdentifier }.isEmpty
+        
+        if !isRunning {
+            let path = Bundle.main.bundlePath as NSString
+            var components = path.pathComponents
+            components.removeLast()
+            components.removeLast()
+            components.removeLast()
+            components.append("MacOS")
+            components.append("Kaka")
+            let newPath = NSString.path(withComponents: components)
+        
+            NSWorkspace.shared.launchApplication(newPath)
+        }
+        else {
+            self.terminate()
+        }
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
